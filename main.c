1aed30a431674bf9f7a7f42d229c6a115d72a40b
Claudia Kukas <c3324631@uon.edu.au>
2020-02-27 16:53:51 +1100

    Initial commit message

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float x;
+    float y;
+    y=x*x/0;
+    printf("Enter a number: ");
+    scanf("%f", &x);
+
+    y = 2;
+    return 0;
+}
